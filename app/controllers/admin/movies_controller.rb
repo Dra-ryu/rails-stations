677766913eeefd5eebc08177b2cee8c447677474
Dra-ryu <64474386+Dra-ryu@users.noreply.github.com>
2022-12-10 13:11:15 +0900

    station3_作成途中

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,26 @@ class Admin::MoviesController < ApplicationController
     def index
         @movies = Movie.all
     end
+
+    def new
+        @movies = Movie.new
+    end
+        
+    def create
+        @movies = Movie.new(movies_params)
+        
+        # puts @movies
+        # puts movies_params
+
+        if @movies.save
+            # puts "aaaaaaaaaaaaaaaaa"
+            redirect_to "/admin/movies"
+        else
+            render action "new"
+        end
+    end
+
+    def movies_params
+        params.permit(:name, :year, :is_showing, :description, :image_url)
+    end
 end
