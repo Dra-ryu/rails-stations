807fc0df05b3fa41910866b3f0101d3695c0f8fc
Dra-ryu <ryunosuke.harada25@gmail.com>
2023-01-23 09:40:44 +0900

    コミットテスト完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,3 @@ Rails.application.routes.draw do
   get 'admin/movies/', to: 'admin/movies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
-# コミットテスト
