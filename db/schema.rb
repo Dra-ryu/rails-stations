39f8a7fb6e056b7eda839d4007e45923a8cd44be
Dra-ryu <ryunosuke.harada25@gmail.com>
2023-01-25 23:02:07 +0900

    null制約の解除

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,11 @@
 ActiveRecord::Schema.define(version: 2019_01_05_082551) do
 
   create_table "movies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", limit: 160, null: false, comment: "映画のタイトル。邦題・洋題は一旦考えなくてOK"
+    t.string "name", limit: 160, comment: "映画のタイトル。邦題・洋題は一旦考えなくてOK"
     t.string "year", limit: 45, comment: "公開年"
     t.text "description", comment: "映画の説明文"
     t.string "image_url", limit: 150, comment: "映画のポスター画像が格納されているURL"
-    t.boolean "is_showing", null: false, comment: "上映中かどうか"
+    t.boolean "is_showing", comment: "上映中かどうか"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_movies_on_name"
