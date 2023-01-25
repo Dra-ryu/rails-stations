05f5925b27c9e48b2df63f9f2bbf7e682065f2bd
Dra-ryu <ryunosuke.harada25@gmail.com>
2023-01-25 23:00:24 +0900

    movies, adminのルーティングをresoucesで設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'movies/', to: 'movies#index'
-  get 'admin/movies/', to: 'admin/movies#index'
+  resources :movies
+  namespace :admin do
+    resources :movies
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
