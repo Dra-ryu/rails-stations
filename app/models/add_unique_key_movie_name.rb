12c635714f28ee21eaf272b6e8236aa5012ee5ec
Dra-ryu <ryunosuke.harada25@gmail.com>
2023-01-27 17:55:46 +0900

    nameカラムにユニーク制約を追加

---
+++ app/models/add_unique_key_movie_name.rb
@@ -0,0 +1,2 @@
+class AddUniqueKeyMovieName < ApplicationRecord
+end
---
+++ db/migrate/20230127010345_create_add_unique_key_movie_names.rb
@@ -0,0 +1,5 @@
+class CreateAddUniqueKeyMovieNames < ActiveRecord::Migration[6.1]
+  def change
+    add_index :movies, [:name], unique: true
+  end
+end
