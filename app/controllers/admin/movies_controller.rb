6b5b8096366050856f9db584dc35610f92c1ec04
Dra-ryu <ryunosuke.harada25@gmail.com>
2023-01-31 16:16:42 +0900

    paramsの設定をcreateとupdateアクションで別々に設定

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,6 +1,7 @@
 class Admin::MoviesController < ApplicationController
     def index
         @movies = Movie.all
+        puts @movies
     end
 
     def new
@@ -9,7 +10,7 @@ class Admin::MoviesController < ApplicationController
         
     def create
         # Movieモデルを初期化し、newの中にmovie_paramsを指定
-        @movies = Movie.new(movie_params)
+        @movies = Movie.new(movie_create_params)
         if @movies.save
             redirect_to "/admin/movies"
         else
@@ -23,15 +24,19 @@ class Admin::MoviesController < ApplicationController
 
     def update
         @movies = Movie.find(params[:id])
-        if @movies.update(movie_params)
+        begin 
+            @movies.update(movie_update_params)
             redirect_to "/admin/movies"
-        else
-            render action "edit"
+        rescue
+            redirect_to "/admin/movies"
         end
     end
 
     private
-    def movie_params
+    def movie_create_params
+        params.permit(:name, :year, :is_showing, :description, :image_url, :id)
+    end
+    def movie_update_params
         params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url, :id)
     end
 end
