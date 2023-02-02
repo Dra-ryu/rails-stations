6e4016e3e0358fd96abf2dad24ddd98a8657da33
Dra-ryu <ryunosuke.harada25@gmail.com>
2023-02-02 09:33:56 +0900

    searchアクションへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :movies
   end
+  get "search" => "movies#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
