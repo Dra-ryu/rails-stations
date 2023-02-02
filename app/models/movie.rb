4c4ebba3cca6ed3832502732610461fd91463aac
Dra-ryu <ryunosuke.harada25@gmail.com>
2023-02-02 09:36:41 +0900

    movieモデルにsearchメソッドを定義

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+    def self.search(search)
+        if search
+            Movie.where(['content LIKE ?', "%#{search}%"])
+        else
+            Movie.all
+        end
+    end
 end
