1c0ea43cfc36b9ec7498a3edf327ca41485df663
Dra-ryu <ryunosuke.harada25@gmail.com>
2023-02-02 09:36:01 +0900

    searchアクションを定義

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,10 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def search
+    #Viewのformで取得したパラメータをモデルに渡す
+    @movies = Movie.search(params[:search])
+  end
+
 end
