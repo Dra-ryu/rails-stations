46ddf600ae08d8ababdadae0eb2bb01ea47ec356
Dra-ryu <ryunosuke.harada25@gmail.com>
2023-02-03 08:59:43 +0900

    キーワード検索部分のアクション実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,19 @@
 class MoviesController < ApplicationController
+
+  before_action :set_q, only: [:index, :search]
   def index
     @movies = Movie.all
   end
 
   def search
-    #Viewのformで取得したパラメータをモデルに渡す
-    @movies = Movie.search(params[:search])
+    @results = @q.result
+  end
+
+  private
+
+  def set_q
+    #  params[:q]→検索時にviewファイルから送られるパラメータ
+    @q = Movie.ransack(params[:q])
   end
 
 end
